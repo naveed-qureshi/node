864d5635fb091a56fc948e40714c3819f4d12734
= <=>
2021-11-29 00:24:52 +0500

    Handle routing using http

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.end('Hello this is node server!');
+  const pathUrl = request.url;
+
+  if (pathUrl === '/product' || pathUrl === '/') {
+    response.end('This is product page');
+  }
+  else if (pathUrl ===  '/review') {
+    response.end('this is review page');
+  }
+  else {
+    response.writeHead(400, {
+      'Content-type' : 'text/html',
+      'my-own-header' : 'hello wordl'
+    })
+    response.end('<h1>Page not found</h1>')
+  }
 })
 
 server.listen(8000, '127.0.0.1', () => {
